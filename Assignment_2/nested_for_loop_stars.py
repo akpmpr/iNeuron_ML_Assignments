96c63328e7d726fff2b6c099f21435d0cb2cefbc
Abhishek KP <65501471+akpmpr@users.noreply.github.com>
2021-01-05 15:00:17 +0530

    Update nested_for_loop_stars.py

--- Assignment_2/nested_for_loop_stars.py
+++ Assignment_2/nested_for_loop_stars.py
@@ -10,14 +10,14 @@ for i in range(0, 5):
         
         print("*",end=" ") 
         
-    print("\r") 
+    print("\n") 
 for i in range(5, 1,-1): 
     
     for j in range(0, i-1): 
         
         print("*",end=" ") 
         
-    print("\r") 
+    print("\n") 
    
 
 
