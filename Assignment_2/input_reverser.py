3abaa61583b25752ee84af644188b5e93287d1ba
Abhishek KP <65501471+akpmpr@users.noreply.github.com>
2021-01-05 14:51:41 +0530

    Add files via upload

---
+++ Assignment_2/input_reverser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+o=input("Enter your input to be reversed :")
+o=o[::-1]
+print(o)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment_2/nested_for_loop_stars.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+for i in range(0, 5): 
+    
+    for j in range(0, i+1): 
+        
+        print("*",end=" ") 
+        
+    print("\r") 
+for i in range(5, 1,-1): 
+    
+    for j in range(0, i-1): 
+        
+        print("*",end=" ") 
+        
+    print("\r") 
+   
+
+
+# In[ ]:
+
+
+
+
